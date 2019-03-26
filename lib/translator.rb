43531cdd39717fea0cefc5d95a41fd677bde3ba2
Gino Desangles <blushingbox@yahoo.com>
2019-03-26 15:15:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(pathFile)
   newList = {"get_emoticon" => {}, "get_meaning" => {}}
   
   newListA.each do |x,y|
-    newList['get_meaning']
+    newList['get_meaning'][y][1]
     binding.pry
   end
 end
